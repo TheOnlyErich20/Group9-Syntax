f1442bff1d8a1783745292efcfb93122c9858c34
Siencmd <sinsender0911@gmail.com>
2026-01-25 01:11:26 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBCMV6CerYS4QCnZ52Q1zcF3bpTwUIuK0w",
   authDomain: "darkboard-f4c38.firebaseapp.com",
   projectId: "darkboard-f4c38",
-  storageBucket: "darkboard-f4c38.firebasestorage.app",
+  storageBucket: "darkboard-f4c38.appspot.com", // <- FIXED
   messagingSenderId: "164112373154",
   appId: "1:164112373154:web:57b154d6ff322a50ae4efb",
   measurementId: "G-KKB3PGBJS5"
