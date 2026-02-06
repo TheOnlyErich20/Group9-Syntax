60a1332bd07237ef7ff1d75bf2c889e04aadcc78
Siencmd <sinsender0911@gmail.com>
2026-02-07 02:52:28 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,30 @@
 // Import modular Firebase API
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+import { 
+    getFirestore, 
+    collection, 
+    addDoc, 
+    getDocs, 
+    query, 
+    orderBy, 
+    where, 
+    doc, 
+    updateDoc, 
+    deleteDoc, 
+    getDoc,
+    serverTimestamp 
+} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
 // Your Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyAx3BTNnEDw4AXEkk5GVR5NLeGjsfVAmlA",
-  authDomain: "darkbroads.firebaseapp.com",
-  projectId: "darkbroads",
-  storageBucket: "darkbroads.firebasestorage.app",
-  messagingSenderId: "271741371558",
-  appId: "1:271741371558:web:45372043bfe9d3205ee067",
-  measurementId: "G-SLEM50XYCK"
+    apiKey: "AIzaSyAx3BTNnEDw4AXEkk5GVR5NLeGjsfVAmlA",
+    authDomain: "darkbroads.firebaseapp.com",
+    projectId: "darkbroads",
+    storageBucket: "darkbroads.firebasestorage.app",
+    messagingSenderId: "271741371558",
+    appId: "1:271741371558:web:45372043bfe9d3205ee067",
+    measurementId: "G-SLEM50XYCK"
 };
 
 // Initialize Firebase
@@ -25,5 +38,19 @@ window.firebaseAuth = auth;
 window.firebaseDb = db;
 window.firebaseFirestore = db;
 
-// Export auth to use in other scripts
-export { auth, db };
+// Export all Firestore functions for use in other scripts
+export { 
+    auth, 
+    db, 
+    collection, 
+    addDoc, 
+    getDocs, 
+    query, 
+    orderBy, 
+    where, 
+    doc, 
+    updateDoc, 
+    deleteDoc, 
+    getDoc,
+    serverTimestamp 
+};
